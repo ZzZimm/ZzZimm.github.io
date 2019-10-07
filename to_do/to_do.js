f5fa8314c51c13f9602f13d5bbe86b660f72eee4
Lucas Zimmermann <lucas_zimmermann@Lucass-MacBook-Pro.local>
2019-10-07 19:25:57 +0200

    fixed

--- to_do/to_do.js
+++ to_do/to_do.js
@@ -1,46 +1,63 @@
 var open = false;
 var nbElem = 0;
 var nbStoredElem = parseInt(localStorage.getItem("nbElem"));
-var rank;
+var maxIndex = 0;
+var nbElem = 0;
+var elemList = [];
 
 retrieveStorage();
 
 function retrieveStorage()
 {
-	var i = 0;
-	var j = 0;
+	var elemListStorage = JSON.parse(localStorage.getItem("elemList"));
 
-	if (nbStoredElem)
+	if (!elemListStorage)
+		return ;
+	maxIndex = parseInt(localStorage.getItem("maxIndex"));
+	nbElem = parseInt(localStorage.getItem("nbElem"));
+	retrieveElem(elemListStorage);
+	elemList = elemListStorage;
+}
+
+function clearTasks()
+{
+	document.getElementById("toDoList").innerHTML =
+	"<tr>" +
+			"<th class=\"checkbox\"></th>" +
+			"<th class=\"title\">Title<button onclick = \"sortBy('title')\" class=\"button\">sort</button></th>" +
+			"<th class=\"description\">Description<button onclick = \"sortBy('description')\" class=\"button\">sort</button></th>" +
+			"<th class=\"dueDate\">Due date<button onclick = \"sortBy('date')\" class=\"button\">sort</button></th>" +
+			"<th class=\"actionBox\">Actions</th>" +
+	"</tr>";
+}
+
+function retrieveElem(storage)
+{
+	var i = -1;
+	var newElemHtml = "";
+
+	while (storage[++i])
 	{
-		nbElem = nbStoredElem;
-		while (nbStoredElem--)
-		{
-		while (!localStorage.getItem("task" + (i + j)))
-				j++;
-			retrieveElem(i++ + j);
-		}
-		rank = i - 1 + j;
+		newElemHtml =
+		"<tr id='" + storage[i].id + "'>"+
+			"<td class=checkbox><input type='checkbox' onclick='validateElem(this)'>" +
+			"<td class= 'title'>" + storage[i].title +
+			"<td class= 'description'>" + storage[i].description +
+			"<td class= 'dueDate'><input oninput='setDate(this)' type='date' value = '" + storage[i].date + "'>" +
+			"<td class= 'actionBox'>" +
+				"<img src='img_src/modify.png' onclick='setInput(this)' class='action'></img>" +
+				"<img src='img_src/validate.png' onclick='setFix(this)' class='action'></img>" +
+				"<img src='img_src/delete.png' onclick='removeElem(this)' class='action'></img>" +
+		"</tr>";
+		document.getElementById("toDoList").style.display = "table";
+		document.getElementById("toDoList").innerHTML += newElemHtml;
 	}
 }
 
-
-function retrieveElem(nb)
+function setDate(elem)
 {
-	var elem = JSON.parse(localStorage.getItem("task" + nb));
-	var newElemHtml =
-	"<tr id='task" + nb + "'>"+
-		"<td class=checkbox><input type='checkbox' onclick='validateElem(this)'>" +
-		"<td class= 'title'>" + elem.title +
-		"<td class= 'description'>" + elem.description +
-		"<td class= 'dueDate'><input type='date'>" +
-		"<td class= 'actionBox'>" +
-			"<img src='img_src/modify.png' onclick='setInput(this)' class='action'></img>" +
-			"<img src='img_src/validate.png' onclick='setFix(this)' class='action'></img>" +
-			"<img src='img_src/delete.png' onclick='removeElem(this)' class='action'></img>" +
-	"</tr>";
-
-	document.getElementById("toDoList").style.display = "table";
-	document.getElementById("toDoList").innerHTML += newElemHtml;
+	elemList.find(x => x.id === elem.parentNode.parentNode.id).date = elem.value;
+	localStorage.setItem("elemList", JSON.stringify(elemList));
 }
 
 function auto_grow(element) {
@@ -50,12 +67,52 @@ function auto_grow(element) {
 
 function removeElem(elem)
 {
-	localStorage.removeItem(elem.parentNode.parentNode.id);
-	console.log(elem.parentNode.parentNode.id);
+	var index = elemList.findIndex(x => x.id === elem.parentNode.parentNode.id);
+
 	elem.parentNode.parentNode.innerHTML = "";
 	if (!--nbElem)
 		document.getElementById("toDoList").style.display = "none";
+	elemList.splice(index, 1);
+	localStorage.setItem("elemList", JSON.stringify(elemList));
 	localStorage.setItem("nbElem", nbElem);
+}
+
+function sortBy(attr)
+{
+	clearTasks();
+	if (attr == "title")
+		retrieveElem(elemList.sort(compareTitle));
+	if (attr == "description")
+		retrieveElem(elemList.sort(compareDescription));
+	if (attr == "date")
+		retrieveElem(elemList.sort(compareDate));
+}
+
+function compareTitle(a, b)
+{
+	if (a.title < b.title)
+		return (-1);
+	if (a.title > b.title)
+		return (1);
+	return (0);
+}
+
+function compareDescription(a, b)
+{
+	if (a.description < b.description)
+		return (-1);
+	if (a.description > b.description)
+		return (1);
+	return (0);
+}
+
+function compareDate(a, b)
+{
+	if (a.date < b.date)
+		return (-1);
+	if (a.date > b.date)
+		return (1);
+	return (0);
 }
 
 function validateElem(elem)
@@ -80,13 +137,10 @@ function addElem()
 	var i = 0;
 	var title = window.prompt("Enter task title: ", "Grocery shopping for Max's birthday");
 	var description = window.prompt("Enter task description: ", "Dark Rhum, Ginger Beer & Crackers");
-	var newElemHtml;
 
 	document.getElementById("toDoList").style.display = "table";
-	while (localStorage.getItem("task" + (rank + i)))
-		i++;
-	newElemHtml =
-	"<tr id='task" + (rank + i) + "'>"+
+	var newElemHtml =
+	"<tr id='task" + maxIndex + "'>"+
 		"<td class=checkbox><input type='checkbox' onclick='validateElem(this)'>" +
 		"<td class= 'title'>" + title +
 		"<td class= 'description'>" + description +
@@ -97,9 +151,17 @@ function addElem()
 			"<img src='img_src/delete.png' onclick='removeElem(this)' class='action'></img>" +
 	"</tr>";
 	document.getElementById("toDoList").innerHTML += newElemHtml;
-	localStorage.setItem("task" + (rank + i), "{\"title\": \"" + title + "\", \"description\": \"" + description + "\"}");
+	var element = {
+			id: "task" + maxIndex++,
+			title: title,
+			description: description,
+			date: "dd/mm/yy"
+	};
+	elemList.push(element);
+	localStorage.setItem("elemList", JSON.stringify(elemList));
+	localStorage.setItem("maxIndex", maxIndex);
 	localStorage.setItem("nbElem", ++nbElem);
-	console.log(document.getElementById("toDoList").lastChild.id);
+
 }
 
 function setInput(elem)
@@ -123,12 +185,15 @@ function setFix(elem)
 	var titleElem = elem.parentNode.parentNode.getElementsByClassName("title");
 	var descriptionElem = elem.parentNode.parentNode.getElementsByClassName("description");
 	var	taskId = elem.parentNode.parentNode.id;
+	var i = elemList.findIndex(x => x.id === taskId);
 
 	if (!open)
 		return ;
 	titleElem[0].innerHTML = titleElem[0].firstChild.value;
 	descriptionElem[0].innerHTML = descriptionElem[0].firstChild.value;
 	elem.style.backgroundColor = "white";
-	localStorage.setItem(taskId, "{\"title\": \"" + titleElem[0].innerHTML + "\", \"description\": \"" + descriptionElem[0].innerHTML + "\"}");
+	elemList[i].description = descriptionElem[0].innerHTML;
+	elemList[i].title = titleElem[0].innerHTML;
+	localStorage.setItem("elemList", JSON.stringify(elemList));
 	open = false;
 }
